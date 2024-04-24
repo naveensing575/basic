239d84166c84c89b2ff578ff8791f26f946a2cfe
naveen.singh <naveen.singh@itelasoft.com>
2024-04-24 16:16:51 +0530

    handle form submission and event handling

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
   <h1>Count: {{ count }}</h1>
   <button v-on:click="increment">Increment Count</button>
   <button v-on:click="decrement">Decrement Count</button>
-  <div class="form">
+  <form class="form" @submit="onSubmit">
     <h3>User Input Form</h3>
     <label for="name">Name:</label>
     <input type="text" v-model="formValues.name" placeholder="Name" /><br />
@@ -30,15 +30,83 @@
       <h4>Two-way binding</h4>
       <p>{{ formValues }}</p>
     </div>
-  </div>
-  <div>
-    <h3>Select country</h3>
-    <select id="country" v-model="formValues.country">
-      <option value="India">India</option>
-      <option value="USA">USA</option>
-      <option value="UK">UK</option>
-    </select>
-  </div>
+    <div>
+      <h3>Select country</h3>
+      <select id="country" v-model="formValues.country">
+        <option value="India">India</option>
+        <option value="USA">USA</option>
+        <option value="UK">UK</option>
+      </select>
+    </div>
+    <div>
+      <h3>Select job location</h3>
+      <select id="jobLocation" multiple v-model="formValues.jobLocation">
+        <option value="India">India</option>
+        <option value="USA">USA</option>
+        <option value="UK">UK</option>
+        <option value="Dubai">Dubai</option>
+        <option value="Australia">Australia</option>
+      </select>
+    </div>
+    <div></div>
+    <h3>Remote Work?</h3>
+    <input
+      type="checkbox"
+      id="remoteWork"
+      true-value="true"
+      false-value="false"
+      v-model="formValues.remoteWork"
+    /><label for="remoteWork">Open to remote work</label>
+    <div></div>
+    <h3>Select skillSet</h3>
+    <div>
+      <input
+        type="checkbox"
+        id="html"
+        value="html"
+        v-model="formValues.skillSet"
+      />
+      <label for="html">HTML</label>
+    </div>
+    <div>
+      <input
+        type="checkbox"
+        id="css"
+        value="css"
+        v-model="formValues.skillSet"
+      />
+      <label for="css">CSS</label>
+    </div>
+    <div>
+      <input type="checkbox" id="js" value="js" v-model="formValues.skillSet" />
+      <label for="js">JS</label>
+    </div>
+    <div>
+      <h3>Years of Experience</h3>
+      <input
+        type="radio"
+        id="0-2"
+        value="0-2"
+        v-model="formValues.yearsOfExp"
+      />
+      <label for="0-2">0-2</label>
+      <input
+        type="radio"
+        id="3-5"
+        value="3-5"
+        v-model="formValues.yearsOfExp"
+      />
+      <label for="3-5">3-5</label>
+      <input
+        type="radio"
+        id="6-10"
+        value="6-10"
+        v-model="formValues.yearsOfExp"
+      />
+      <label for="6-10">6-10</label>
+    </div>
+    <button type="submit" class="btn">Submit</button>
+  </form>
 </template>
 
 <script>
@@ -55,6 +123,10 @@ export default {
         name: "",
         email: "",
         country: "",
+        jobLocation: [],
+        remoteWork: false,
+        skillSet: [],
+        yearsOfExp: 0,
       },
       count: 0,
       users: {
@@ -79,6 +151,10 @@ export default {
     decrement() {
       return (this.count -= 1);
     },
+    onSubmit(e) {
+      e.preventDefault();
+      console.log("Thanks you for your registration", this.formValues);
+    },
   },
 };
 </script>
@@ -102,6 +178,16 @@ input {
   border-radius: 5px;
   padding: 5px 10px;
   margin-bottom: 10px;
+}
+.btn {
+  padding: 5px 10px;
+  font-size: large;
+  margin-top: 20px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  color: black;
+  background: #1df0ff;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
